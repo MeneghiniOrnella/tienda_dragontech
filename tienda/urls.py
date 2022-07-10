307435ce8ff0cc157a1c36b77551d06805acbbb7
COM <meneghini.ornella@gmail.com>
2022-07-10 11:24:31 -0300

    Lenovo's Backup

--- tienda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import persona_listar
-
-urlpatterns = [
-    path("personas",persona_listar,name='persona_listar'),
-    path("nueva_persona",views.nueva_persona,name="nueva_persona"),
-    path("modificar_personas/<int:pk>", views.modoficar_persona, views.modoficar_persona, name="modificar_personas_1")
-]
